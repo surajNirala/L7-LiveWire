17d5bf0a98ee08221ab1ad4c147d12c896808b18
surajniral <suraj@diverseinfotech.com>
2020-05-26 16:23:37 +0530

    Livewire Comments System

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -3,11 +3,30 @@
 namespace App\Http\Livewire;
 
 use App\Comment;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
     
+    public $comments = [
+        [
+        "creator" => "Suraj Nirala",
+        "created_at" => "2 mint ago",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae blanditiis reprehenderit magnam est atque saepe deserunt aspernatur quod architecto vel et nobis laudantium inventore ad, eligendi in modi error dignissimos!",
+        ],
+    ];
+
+    public function addComment()
+    {
+        array_unshift($this->comments,[
+            "creator" => "Raj",
+            "created_at" => Carbon::now()->diffForHumans(),
+            "body" => $this->newComment,
+        ]);
+        $this->newComment = "";
+    }
+    public $newComment;
     public function render()
     {
        
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,71 +1,31 @@
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <form action="" method="post">
-                <div class="form-row">
-                  <div class="col-3">
-                <input type="text" name="title" placeholder="Title" class="form-control">
-              </div>
-              <div class="col-3">
-                <input type="file" name="file" placeholder="Title" class="form-control">
-              </div>
-              <div class="col-4">
-                <textarea name="comment" id="comment" class="form-control" placeholder="Comment"></textarea>
-              </div>
-              <div class="col-2">
-                <input type="submit"  name="submit" placeholder="Title" class="btn btn-success">
+<div class="container">
+  {{-- <form> --}}
+    <div class="row">
+      <div class="col-9">
+        <input type="text" class="form-control" placeholder="Comments" wire:model="newComment">
+      </div>
+      <div class="col-3">
+        <input type="submit" class="form-control btn btn-primary" wire:click="addComment" value="Add">
+      </div>
+    </div>
+  {{-- </form>   --}}
+  <br>   
+  {{$newComment}}
+      <div class="card">
+        <div class="card-body">
+          @foreach($comments as $key => $comment)
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="https://image.ibb.co/jw55Ex/def_face.jpg" width="100" height="100" class="img img-rounded img-fluid"/>
+                <p class="text-secondary text-center">{{$comment['created_at']}}</p>
                 </div>
-              </div>
-            </form>
-            <div class="card">
-                <div class="card-header">
-                  Comments
+                <div class="col-md-10">
+                  <p><strong>{{$comment['creator']}}</strong></p>
+                   <div class="clearfix"></div>
+                    <p>{{$comment['body']}}</p>
                 </div>
-                <div class="card-body">
-                  <div class="row">
-                  <div class="col-md-4">
-                      <img  src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" class="card-img" alt="...">
-                  </div>
-                  <div class="col-md-8">
-                      <h5 class="card-title">dwefcerfr</h5>
-                  <p class="card-text">vfevfvfvf</p>
-                      <p class="card-text"><small class="text-muted">3 min ago</small></p>
-                  </div>
-                  </div>
-              </div>
             </div>
-        </div>
-        <div class="col-md-6">
-            <table class="table table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
-        </div>
+            @endforeach
+          </div>
     </div>
 </div>
